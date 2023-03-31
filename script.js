94438a21ed524c8e588336d791e23c1006dbd886
dgomez04 <dagomezmoreno@outlook.com>
2023-03-31 12:34:26 -0600

    Finished playGame function, working version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//player choices for r, p , s
 const choices = ["Rock", "Paper", "Scissors"];
 
 
@@ -34,17 +34,37 @@ function getPlayerSelection() {
 function playRound(playerSelection, computerSelection) {
     //ties, computer wins, player wins.
     if(computerSelection === playerSelection ) {
-        console.log("It's a tie!");
+        return("TIE");
     } else if ((computerSelection === "Rock" && playerSelection === "Scissors") || (computerSelection === "Scissors" && playerSelection === "Paper") || (computerSelection === "Paper" && playerSelection === "Rock") ) {
-        console.log("Computer wins!");
+        return("COMPUTER WIN");
     } else {
-        console.log("You win!");
+        return("PLAYER WIN");
     }
 
 }
 
 /* plays r, p, s until someone wins the game*/
 function playGame() {
-    let score;
+    //score attributes
+    let scoreComputer= 0;
+    let scorePlayer= 0;
+
+    //while either score attributes aren't five, play game 
+    while(!(scoreComputer == 5 || scorePlayer == 5)) {
+        //create a variable in order to manipulate playRound's return value.
+        let round = playRound(getPlayerSelection(), getComputerSelection());
+        console.log(round);
+
+        if(round === "COMPUTER WIN") {
+            scoreComputer++;
+            console.log("Computer score: " + scoreComputer);
+        } else if (round === "PLAYER WIN") {
+            scorePlayer++;
+            console.log("Player score: " + scorePlayer);
+        } 
+
+    }
 }
 
+//runs the game.
+playGame();
